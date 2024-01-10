696f157bffe660ddb910e76399f90cbf73f40a27
Vamsi-Krishna <vamsitupakula474@gmail.com>
2024-01-10 14:59:56 +0530

    routes added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="/beta/signup" element={<SignUp />} />
+        <Route path="/beta/login" element={<Login />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl">SignUp Page</h1>
+    </div>
+  );
+}
+
+export default SignUp;
