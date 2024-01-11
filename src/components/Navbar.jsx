c43f9079ef885c9ef1948b67d641e9455f0b7cd0
Vamsi-Krishna <vamsitupakula474@gmail.com>
2024-01-11 10:44:26 +0530

    navbar responsiveness added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div className="fixed w-[100vw] flex justify-center z-10">
-      <div className="flex align-middle justify-between w-[100vw] max-w-[1570px] text-white px-4 md:px-8 lg:px-16 py-4 h-[49px] md:h-[105px]">
+    <div className="fixed w-[100vw] flex justify-center z-10 ">
+      <div className="flex align-middle justify-between w-[100vw] max-w-[1570px] text-white px-4 md:px-8 lg:px-16 py-4">
         <div>
           <a className="" href="/">
             <img
@@ -15,28 +15,34 @@ function Navbar() {
               height={100}
               decoding="async"
               data-nimg={1}
-              className="w-[112px] h-[43px] md:w-[186px] md:h-[71px]"
+              className="w-[186px] h-[71px]"
               style={{ color: "transparent" }}
               src={NeatSkillsLogo}
             />
           </a>
         </div>
-        <ul className="flex items-center justify-between w-full max-w-[407px] mx-10 lg:mx-12">
+        <ul className="flex items-center justify-between max-w-[407px] mx-5 flex-col lg:flex-row lg:mx-12 gap-2 lg:gap-6 py-5 lg:py-0 px-3">
           <a href="/beta/courseoverview">
-            <li className="text-md uppercase hover:border-b  ">Courses</li>
+            <li className="text-md uppercase hover:border-b lg:mx-2">
+              Courses
+            </li>
           </a>
           <a href="/alpha/aboutus">
-            <li className="text-md uppercase hover:border-b  ">About Us</li>
+            <li className="text-md uppercase hover:border-b lg:mx-2">
+              About Us
+            </li>
           </a>
           <a href="/alpha/contactus">
-            <li className="text-md uppercase hover:border-b  ">Contact</li>
+            <li className="text-md uppercase hover:border-b lg:mx-2">
+              Contact
+            </li>
           </a>
         </ul>
-        <div className="flex items-center gap-[2rem]">
+        <div className="flex items-center gap-[10px] lg:gap-[2rem] flex-col lg:flex-row">
           <NavLink to="/beta/signup">
             <button
               type="button"
-              className="bg-pink-600 uppercase border border-[#ffffff] rounded-lg w-[77px] h-7 md:text-sm md:w-24 md:h-11 lg:text-xl lg:w-32 lg:h-14 hover:scale-[1.15] transition-transform duration-300"
+              className="bg-pink-600 uppercase border border-[#ffffff] rounded-lg px-6 text-nowrap py-3  hover:scale-[1.15] transition-transform duration-300"
             >
               Sign up
             </button>
@@ -44,7 +50,7 @@ function Navbar() {
           <NavLink to="/beta/login">
             <button
               type="button"
-              className="uppercase border border-[#ffffff] rounded-lg w-[77px] h-7 md:text-sm md:w-24 md:h-11 lg:text-xl lg:w-32 lg:h-14 hover:scale-[1.15] transition-transform duration-300"
+              className="uppercase border border-[#ffffff] rounded-lg px-6 text-nowrap py-3 hover:scale-[1.15] transition-transform duration-300"
             >
               LOGIN
             </button>
